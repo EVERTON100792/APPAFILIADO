07f68a260bbcf1740cc62cb6ecaccfdefb00022f
EVERTON100792 <luismouraeverton@gmail.com>
2026-04-04 01:21:02 -0300

    Adiciona erro global para debug

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+window.addEventListener('error', (e) => {
+  console.error('Global error:', e.error);
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
